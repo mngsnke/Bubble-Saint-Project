56e039ae5311b20fdfdf85d3f9bb9feb9949e8b2
mngsnke <32736475+mngsnke@users.noreply.github.com>
2020-11-30 07:15:36 +0400

    Updated Boba Properties

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function setup() {
     canvas.style('z-index', '-1');
     
     //frame rate set to 30.
-    frameRate(144);
+    frameRate(60);
     
     //canvas position lcoked
     canvas.position(0, 0, 'fixed');
@@ -26,8 +26,8 @@ function setup() {
     //milktea initialization
     milktea = new MilkTea(-100, 0, yoff, c, strOff);
     
-    for(var i = 0; i < 10; i++) {
-        boba[i] = new Boba(3, random(0, windowWidth), random(-150, -100));
+    for(var i = 0; i < 100; i++) {
+        boba[i] = new Boba(20, random(0, windowWidth), random(0, 100));
     }
     
 }
@@ -42,23 +42,20 @@ function draw() {
     fill('#3f1c00');
     
     
-    for(var x = 0; x < boba.length; x++) {
-        let gravity = createVector(0, 0.1 * boba[x].mass);
+    for(var i = 0; i < boba.length - 1; i++) {
+        let gravity = createVector(0, 0.1 * boba[i].mass);
         
-        if(boba[x].position.y >= windowHeight - boba[x].mass * 16) {
-            boba[x].velocity.y *= -0.9;
+        if(boba[i].position.y >= windowHeight - boba[i].mass) {
+            boba[i].velocity.y *= 0;
             
         } else {
-            boba[x].applyForce(gravity);
-            boba[x].update();
+            boba[i].applyForce(gravity);
+            boba[i].update();
         }
         
-        boba[x].display();
+        boba[i].display();
         
     }
-    
-    
-    
 }
 
 //Boba class with mass/size, x and y parameters.
@@ -76,6 +73,12 @@ Boba.prototype.applyForce = function(force) {
     this.acc.add(f);
 }
 
+Boba.prototype.checkCollision = function(other) {
+    if((this.position.y) > (other.position.y)) {
+        this.position.y = this.position.y - (this.position.y - other.position.y);
+    }
+}
+
 
 /*
 Boba.prototype.checkSubmerged = function() {
@@ -86,7 +89,7 @@ Boba.prototype.checkSubmerged = function() {
 Boba.prototype.display = function() {
     noStroke();
     fill('#3f1c00');
-    ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
+    ellipse(this.position.x, this.position.y, this.mass * 2, this.mass * 2);
 }
 
 Boba.prototype.update = function() {
@@ -116,8 +119,8 @@ MilkTea.prototype.display = function() {
     
     beginShape();
     //initial value for x is same as strOff to match where the tea starts on left side.
-    for (var x = this.x; x <= windowWidth+this.strOff; x+=1) {
-        var nx = map(x, 0, windowWidth, 0, 0.2);
+    for (var x = this.x; x <= windowWidth+this.strOff; x+=100) {
+        var nx = map(x, 0, windowWidth, 0, 0.6);
         var y = 100 * noise(nx + this.yOff) + c;
         vertex(x,y);
     }
